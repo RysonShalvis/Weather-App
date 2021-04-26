e02a85f9f79db9b0912a646c7f3a2169b52bd089
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2021-04-26 01:23:25 -0600

    compare temps of to cities

--- src/App.js
+++ src/App.js
@@ -21,13 +21,25 @@ class App extends Component {
     }
 
     render() {
+
+        const compareData = () => {
+
+            const farenheight1 = (this.state.data1.main.temp - 273.15) * (9/5) + 32;
+            const farenheight2 = (this.state.data2.main.temp - 273.15) * (9/5) + 32;
+
+            if (farenheight1 > farenheight2) {
+                return <p>{this.state.data1.name} is {Math.round(Number(farenheight1) - Number(farenheight2))} °F warmer than {this.state.data2.name}</p>
+            }
+            return <p>{this.state.data2.name} is {Math.round(Number(farenheight2) - Number(farenheight1))} °F warmer than {this.state.data1.name}</p>
+        }
+
         return (
             <div className="">
                 <div className="weatherapp-ctn">
                     <WeatherApp childData={this.getChildData}/>
                     <WeatherApp childData={this.getChildDataTwo} />
                 </div>
-                {this.state.data1 && this.state.data2 ? <p className="">{this.state.data1.name} {this.state.data2.name}</p> : ""}
+                {this.state.data1 && this.state.data2 ? compareData() : ""}
             </div>
         );
     }
